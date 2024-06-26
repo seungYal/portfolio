082382daa567302e98ca006d71d118706b6a3a7c
승얄 <164878414+seungYal@users.noreply.github.com>
2024-06-26 12:23:52 +0900

    html, js 수정
    자기소개란 수정
    totop 버튼, 네비게이션 클릭 시 스크롤 이벤트 추가

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function skillMenu(a) {
   }
   list[a].classList.add('target');
 }
+
+function goToScroll(name) {
+  var location = document.querySelector('.' + name).offsetTop;
+  window.scrollTo({ top: location - 10, behavior: 'smooth' });
+}
+
+function toTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 500) {
+    document.querySelector('.toTop').style.display = 'block';
+  } else {
+    document.querySelector('.toTop').style.display = 'none';
+  }
+});
+
+window.addEventListener('scroll', function () {
+  let navi = document.querySelector('.navi');
+  if (window.scrollY > 0) {
+    navi.classList.add('scroll');
+  } else {
+    navi.classList.remove('scroll');
+  }
+});
